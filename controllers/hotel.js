fc043a3382544176905114966fbab3cf0fd6d56d
manutdmohit <saudmohit@gmail.com>
2022-03-08 09:11:34 +0545

    hotel controller

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,6 +1,16 @@
+const res = require('express/lib/response');
 const { StatusCodes } = require('http-status-codes');
 
 const Hotel = require('../models/Hotel');
+
+// @desc Get All Hotels
+// @route GET /api/v1/hotels
+// @access Public
+exports.createHotel = async (req, res) => {
+  const hotel = await Hotel.create(req.body);
+
+  res.status(StatusCodes.OK).json({ hotel });
+};
 
 // @desc Get All Hotels
 // @route GET /api/v1/hotels
@@ -10,7 +20,7 @@ exports.getHotels = async (req, res) => {
   res.status(StatusCodes.OK).json({ count: hotel.length, hotel });
 };
 
-// @desc Get All Hotels
+// @desc Get Single Hotel
 // @route GET /api/v1/hotels/:slug
 // @access public
 exports.getHotel = async (req, res) => {
