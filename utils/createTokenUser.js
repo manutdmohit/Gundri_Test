845b4749cb901d5351afee636534aca0175459f0
manutdmohit <saudmohit@gmail.com>
2022-03-10 09:44:56 +0545

    Create Token User

--- utils/createTokenUser.js
+++ utils/createTokenUser.js
@@ -1,5 +1,14 @@
 const createTokenUser = (user) => {
-  return { name: user.name, userId: user._id, role: user.role };
+  return {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    userId: user._id,
+    country: user.country,
+    mobileNumber: user.mobileNumber,
+    address1: user.address1,
+    address2: user.address2,
+  };
 };
 
 module.exports = createTokenUser;
