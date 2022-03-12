ac537b5ce388adad98ddce202bd495b95e3e686f
mohitverse <mohit@prabidhilabs.com>
2022-03-12 09:26:43 +0545

    Admin Controller

--- controllers/accounts/adminController.js
+++ controllers/accounts/adminController.js
@@ -9,8 +9,6 @@ const Customer = require('../../models/Accounts/Customer');
 const GuestCustomer = require('../../models/Accounts/GuestCustomer');
 const Partner = require('../../models/Accounts/Partner');
 
-const { createGuestCustomer } = require('./guestcustomerController');
-
 // @desc Create Admin
 // @route POST /api/v1/admins
 // @access Private
@@ -37,4 +35,40 @@ exports.createAdmin = async (req, res) => {
   attachCookiesToResponse({ res, user: tokenUser });
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
+};
+
+exports.loginAdmin = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Please provide email and password');
+  }
+
+  const user = await Admin.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+
+  // Generating Token
+  const tokenUser = createTokenUser(user);
+
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+
+exports.logoutAdmin = async (req, res) => {
+  res.cookie('token', '', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
