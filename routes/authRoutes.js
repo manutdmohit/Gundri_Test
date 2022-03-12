c931886c5c2feb0951eaf61bfec7c4d4fe69bbeb
mohitverse <mohit@prabidhilabs.com>
2022-03-12 14:59:38 +0545

    adding guest and partner routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, logout } = require('../controllers/authController');
+const {
+  register,
+  login,
+  logout,
+  registerGuest,
+  registerPartner,
+} = require('../controllers/authController');
 
 router.post('/register', register);
+router.post('/register/guest', registerGuest);
+router.post('/register/partner', registerPartner);
 router.post('/login', login);
 router.get('/logout', logout);
 
