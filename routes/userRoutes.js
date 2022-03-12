43f0a2935e427de49c7d0c40fc5991aab6c57a49
mohitverse <mohit@prabidhilabs.com>
2022-03-12 09:39:35 +0545

    Main User Routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const {
+  getAllUsers,
+  getAllCustomers,
+  getAllGuests,
+  getAllPartners,
+  getSingleUser,
+  showCurrentLoggedInUser,
+  updateUser,
+  updateUserPassword,
+} = require('../controllers/userController');
+
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(authenticateUser, authorizePermissions('admin'), getAllUsers);
+
+router
+  .route('/customers')
+  .get(authenticateUser, authorizePermissions('admin'), getAllCustomers);
+
+router
+  .route('/guests')
+  .get(authenticateUser, authorizePermissions('admin'), getAllGuests);
+
+router
+  .route('/partners')
+  .get(authenticateUser, authorizePermissions('admin'), getAllPartners);
+
+router.route('/showme').get(authenticateUser, showCurrentLoggedInUser);
+
+router.route('/updateuserpassword').patch(authenticateUser, updateUserPassword);
+
+router.route('/updateuser/:id').patch(authenticateUser, updateUser);
+
+router.route('/:id').get(authenticateUser, getSingleUser);
+
+module.exports = router;
