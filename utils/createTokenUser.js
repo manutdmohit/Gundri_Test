2fef210e61a7a2813d62c11c839db59207a6cdbe
mohitverse <mohit@prabidhilabs.com>
2022-03-12 15:00:33 +0545

    Added more functionality to createTokenUser

--- utils/createTokenUser.js
+++ utils/createTokenUser.js
@@ -1,12 +1,45 @@
 const createTokenUser = (user) => {
-  return {
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    role: user.role,
-    userId: user._id,
-    mobileNumber: user.mobileNumber,
-  };
+  if (user.address && user.country) {
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+      userId: user._id,
+      mobileNumber: user.mobileNumber,
+      address: user.address,
+      country: user.country,
+    };
+  } else if (
+    user.address1 &&
+    user.address2 &&
+    user.state &&
+    user.city &&
+    user.country
+  ) {
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+      userId: user._id,
+      mobileNumber: user.mobileNumber,
+      address1: user.address1,
+      address2: user.address2,
+      state: user.state,
+      city: user.city,
+      country: user.country,
+    };
+  } else {
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+      userId: user._id,
+      mobileNumber: user.mobileNumber,
+    };
+  }
 };
 
 module.exports = createTokenUser;
