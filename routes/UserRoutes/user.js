d9e72f50f36fb079ad4e5786bc2054d1e95aca07
mohitverse <mohit@prabidhilabs.com>
2022-03-12 09:39:17 +0545

    User Routes

---
+++ routes/UserRoutes/user.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {
+  getAllCustomers,
+  getAllGuestCustomers,
+  getAllPartners,
+  getSingleCustomer,
+  getSingleGuestCustomer,
+  getSinglePartner,
+  deleteCustomer,
+  deleteGuestCustomer,
+  deletePartner,
+  updateCustomer,
+  updateCustomerPassword,
+} = require('../../controllers/users/userController');
+
+const {
+  showCurrentLoggedInUser,
+} = require('../../controllers/getCurrentLoggedInUser');
+
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../../middleware/authentication');
+
+router.route('/customers').get(authenticateUser, getAllCustomers);
+router.route('/guests').get(authenticateUser, getAllGuestCustomers);
+router.route('/partners').get(authenticateUser, getAllPartners);
+
+router.route('/showMe').get(authenticateUser, showCurrentLoggedInUser);
+
+router
+  .route('/customers/updatecustomerpassword')
+  .patch(authenticateUser, updateCustomerPassword);
+
+router
+  .route('/customers/:id')
+  .get(authenticateUser, getSingleCustomer)
+  .delete(authenticateUser, deleteCustomer)
+  .patch(authenticateUser, updateCustomer);
+
+router
+  .route('/guests/:id')
+  .get(authenticateUser, getSingleGuestCustomer)
+  .delete(authenticateUser, deleteGuestCustomer);
+
+router
+  .route('/partners/:id')
+  .get(authenticateUser, getSinglePartner)
+  .delete(authenticateUser, deletePartner);
+
+module.exports = router;
