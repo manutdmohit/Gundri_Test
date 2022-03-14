3c5d1542fb2e6bf7366e1ce2687658d455373f90
mohitverse <mohit@prabidhilabs.com>
2022-03-14 16:07:08 +0545

    Added getSingleStay, UpdateStay and Delete Stay

--- controllers/stayController.js
+++ controllers/stayController.js
@@ -4,6 +4,8 @@ const CustomError = require('../errors');
 
 const Stay = require('../models/Stay');
 
+const { checkPermissions } = require('../utils');
+
 // @desc Create Stay(Hotel)
 // @route POST /api/v1/hotels/create
 // @access Private
@@ -18,7 +20,7 @@ exports.createStay = async (req, res) => {
     );
   }
 
-  const stay = await Stay.create(req.body);
+  const stay = await Stay.create({ ...req.body, createdBy: req.user.userId });
 
   res.status(StatusCodes.CREATED).json({
     stay,
@@ -28,11 +30,52 @@ exports.createStay = async (req, res) => {
 // @desc Get All Stays
 // @route GET /api/v1/hotels
 // @access Public
-exports.getHotels = async (req, res) => {
+exports.getStays = async (req, res) => {
   const stays = await Stay.find({});
 
   res.status(StatusCodes.OK).json({
     count: stays.length,
     stays,
   });
+};
+
+exports.getSingleStay = async (req, res) => {
+  const stay = await Stay.findOne({
+    _id: req.params.id,
+  }).select('-createdBy');
+
+  if (!stay) {
+    throw new CustomError.NotFoundError(`No stay with id ${req.params.id}`);
+  }
+
+  res.status(StatusCodes.OK).json({ stay });
+};
+
+exports.updateStay = async (req, res) => {
+  const stay = await Stay.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!stay) {
+    throw new CustomError.NotFoundError('No stay found');
+  }
+
+  checkPermissions(req.user, stay.createdBy);
+
+  res.status(StatusCodes.OK).json({ stay });
+};
+
+exports.deleteStay = async (req, res) => {
+  const stay = await Stay.findOne({ _id: req.params.id });
+
+  if (!stay) {
+    throw new CustomError.NotFoundError(`No stay with id ${req.params.id}`);
+  }
+
+  checkPermissions(req.user, stay.createdBy);
+
+  await stay.remove();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Stay removed.' });
 };
