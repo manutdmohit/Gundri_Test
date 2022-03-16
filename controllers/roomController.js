1a45188d112fe0e2350c8d370a301cc43117139d
mohitverse <mohit@prabidhilabs.com>
2022-03-16 06:49:44 +0545

    add, get, update, delete room

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -7,6 +7,8 @@ const Room = require('../models/Room');
 
 const Stay = require('../models/Stay');
 
+const { checkPermissions } = require('../utils');
+
 // @desc Create Room
 // @route POST /api/v1/rooms
 // @access Private
@@ -28,8 +30,53 @@ exports.createRoom = async (req, res) => {
   });
 };
 
-exports.getRooms = async (req, res) => {
+exports.getAllRooms = async (req, res) => {
   const rooms = await Room.find({});
 
-  res.status(StatusCodes.OK).json(rooms);
+  res.status(StatusCodes.OK).json({ count: rooms.length, rooms });
+};
+
+exports.getSingleRoom = async (req, res) => {
+  const room = await Room.findOne({ _id: req.params.id });
+
+  if (!room) {
+    throw new CustomError.NotFoundError(
+      `No room found with id ${req.params.id}`
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ room });
+};
+
+exports.updateRoom = async (req, res) => {
+  const room = await Room.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!room) {
+    throw new CustomError.NotFoundError(
+      `No room found with id ${req.params.id}`
+    );
+  }
+
+  checkPermissions(req.user, room.createdBy);
+
+  res.status(StatusCodes.OK).json({ room });
+};
+
+exports.deleteRoom = async (req, res) => {
+  const room = await Room.findOne({ _id: req.params.id });
+
+  if (!room) {
+    throw new CustomError.NotFoundError(
+      `No room found with id ${req.params.id}`
+    );
+  }
+
+  checkPermissions(req.user, room.createdBy);
+
+  await room.remove();
+
+  res.status(StatusCodes.OK).json({ msg: 'room deleted' });
 };
