57896303c0cf85e92d303c745c460ea0635d5d62
mohitverse <mohit@prabidhilabs.com>
2022-03-16 06:50:09 +0545

    Room Routes

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-const { createRoom } = require('../controllers/roomController');
+const {
+  createRoom,
+  getAllRooms,
+  getSingleRoom,
+  updateRoom,
+  deleteRoom,
+} = require('../controllers/roomController');
+
 const {
   authenticateUser,
   authorizePermissions,
 } = require('../middleware/authentication');
+
+router.get('/', getAllRooms);
 
 router.post(
   '/create',
@@ -14,5 +23,19 @@ router.post(
   authorizePermissions('admin', 'partner'),
   createRoom
 );
+
+router.route('/:id').get(getSingleRoom);
+
+router
+  .route('/update/:id')
+  .patch(
+    authenticateUser,
+    authorizePermissions('admin', 'partner'),
+    updateRoom
+  );
+
+router
+  .route('/delete/:id')
+  .delete(authenticateUser, authorizePermissions('admin'), deleteRoom);
 
 module.exports = router;
