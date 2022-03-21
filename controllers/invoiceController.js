e40d72a21aa3f19ee1ad63b00526f23203708d65
mohitverse <mohit@prabidhilabs.com>
2022-03-21 11:33:12 +0545

    Invoice Controller

---
+++ controllers/invoiceController.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const Room = require('../models/Room');
+const Booking = require('../models/Booking');
+
+// @desc Create Invoice
+// @route POST /api/v1/invoice
+// @access Private
+
+const { StatusCodes } = require('http-status-codes');
+
+const createInvoice = async (req, res) => {
+  const user = await User.findOne({ _id: req.user.userId }).select('-password');
+
+  const booking = await Booking.findOne({ bookedBy: req.user.userId });
+
+  const room = await Room.findOne({ _id: booking.room_type });
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      bookingNumber: booking.id,
+      firstName: user.firstName,
+      address: user.address,
+      mobileNumber: user.mobileNumber,
+    });
+};
+
+module.exports = createInvoice;
