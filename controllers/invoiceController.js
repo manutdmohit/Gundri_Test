c5feafa81df6abffaf230f6b09b492fb6d774408
mohitverse <mohit@prabidhilabs.com>
2022-03-24 08:35:02 +0545

    Invoice Controller

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -1,13 +1,16 @@
-// @desc Create Invoice
-// @route POST /api/v1/invoice
-// @access Private
+const { StatusCodes } = require('http-status-codes');
+
+const CustomError = require('../errors');
+
 const User = require('../models/User');
 const Room = require('../models/Room');
 const Booking = require('../models/Booking');
 const Stay = require('../models/Stay');
 const Invoice = require('../models/Invoice');
 
-const { StatusCodes } = require('http-status-codes');
+// @desc Create Invoice
+// @route POST /api/v1/invoice
+// @access Private
 
 const createInvoice = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId }).select('-password');
@@ -16,15 +19,28 @@ const createInvoice = async (req, res) => {
     _id: req.params.id,
   });
 
+  console.log(booking);
+  if (!booking) {
+    throw new CustomError.NotFoundError(
+      `No Booking found with id ${req.params.id}`
+    );
+  }
+
   let stay = await Stay.findOne({ stay: booking.stay });
 
   let rooms = booking.rooms;
 
-  const totalAmount = rooms.reduce((prev, curr) =>
-    Number(prev.totalPrice + curr.totalPrice, 0)
-  );
+  const invoiceCount = await Invoice.find();
 
-  console.log(totalAmount);
+  if (invoiceCount.length > 0) {
+    const data = await Invoice.findOne({ bookingNumber: req.params.id });
+
+    const bookingNumber = data.bookingNumber.toString();
+
+    if (bookingNumber === req.params.id) {
+      throw new CustomError.BadRequestError('Booking already present');
+    }
+  }
 
   const invoice = await Invoice.create({
     bookingNumber: booking.id,
@@ -35,9 +51,10 @@ const createInvoice = async (req, res) => {
     staylocation: stay.location,
     mobileNumber: user.mobileNumber,
     roomType: rooms,
-    totalAmount,
+    totalAmount: booking.totalAmount,
+    checkIn: booking.checkIn,
+    checkOut: booking.checkOut,
   });
-
   res.status(StatusCodes.CREATED).json({
     invoice,
   });
